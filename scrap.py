fd9f0b6c447190a8cc6231d636813947a783e8be
linted <linted@users.noreply.github.com>
2019-01-04 23:06:10 -0500

    lots of updates. figured out how to do the set operations

--- scrap.py
+++ scrap.py
@@ -2,6 +2,8 @@
 import collections
 import json
 import re
+import threading
+import time
 
 import requests
 
@@ -11,67 +13,39 @@ searches = [
     r"[\s:]*([59][0-9a-zA-Z]{50})[\s]*" #Base58 Wallet Import format
 ]
 
-pastbin_scrape_url = "https://scrape.pastebin.com/api_scraping.php"
-pastbin_scrape_params = {"limit":"100"}
-
-class OrderedSet(collections.OrderedDict, collections.MutableSet):
-    def __init__(self, *args):
-        super.__init__()
-        self.update(*args)
-
-    def update(self, *args, **kwargs):
-        if kwargs:
-            raise TypeError("update() takes no keyword arguments")
-
-        for s in args:
-            for e in s:
-                 self.add(e)
-
-    def add(self, elem):
-        self[elem] = None
-
-    def discard(self, elem):
-        self.pop(elem, None)
-
-    def __le__(self, other):
-        return all(e in other for e in self)
-
-    def __lt__(self, other):
-        return self <= other and self != other
-
-    def __ge__(self, other):
-        return all(e in self for e in other)
-
-    def __gt__(self, other):
-        return self >= other and self != other
-
-    def __repr__(self):
-        return 'OrderedSet([%s])' % (', '.join(map(repr, self.keys())))
-
-    def __str__(self):
-        return '{%s}' % (', '.join(map(repr, self.keys())))
-
-    difference = property(lambda self: self.__sub__)
-    difference_update = property(lambda self: self.__isub__)
-    intersection = property(lambda self: self.__and__)
-    intersection_update = property(lambda self: self.__iand__)
-    issubset = property(lambda self: self.__le__)
-    issuperset = property(lambda self: self.__ge__)
-    symmetric_difference = property(lambda self: self.__xor__)
-    symmetric_difference_update = property(lambda self: self.__ixor__)
-    union = property(lambda self: self.__or__)
+pastbin_listing_url = "https://scrape.pastebin.com/api_scraping.php"
+pastbin_listing_params = {"limit":"100"}
+pastbin_scrape_url = "https://scrape.pastebin.com/api_scrape_item.php?i="
 
 
 def main():
-    pass
+    old_listing = set()
+    current_listing = set()
+    new_elements = set()
+    while (1):
+        current_listing = get_updates() # slow operation, but it has to be done
+        new_elements = current_listing - old_listing
+        old_listing = current_listing
+
+        for item in new_elements:
+            threading.Thread(target=parse_paste, args=(item,), daemon=True)
+
+        time.sleep(1) #sleep for a second no matter what. pastes come in slow most of the time
 
 def get_updates():
-    r = requests.get(url=pastbin_scrape_url, params=pastbin_scrape_params)
-    if r.status_code == 200:
-        
+    ret = set()
+    listing = requests.get(url=pastbin_listing_url, params=pastbin_listing_params)
+    if listing.status_code == 200:
+        pastes = json.loads(listing.text)
+        for items in pastes:
+            ret.add(items["key"])
     else:
-        raise ConnectionError("Status code: {}".format(r.status_code))
-    
+        raise ConnectionError("Status code: {}".format(listing.status_code))
+    return ret
+
+def parse_paste():
+    pass
+
 
 if __name__ == "__main__":
     main()
