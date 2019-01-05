64e5f68152e9705827b8effd5296d1ee7a7e5eed
linted <linted@users.noreply.github.com>
2019-01-04 23:22:03 -0500

    moving forward. ever marching onward

--- scrap.py
+++ scrap.py
@@ -2,20 +2,22 @@
 import collections
 import json
 import re
+import smtplib
+import ssl
 import threading
 import time
 
 import requests
 
-searches = [
-    r"[\s:]*([0-9a-zA-Z]{64})[\s]*", #btc private key
-    r"[\s:]*([0-9a-zA-Z]{32})[\s]*", #btc address
-    r"[\s:]*([59][0-9a-zA-Z]{50})[\s]*" #Base58 Wallet Import format
-]
+searches = {
+    r"[\s:]*([0-9a-zA-Z]{64})[\s]*": "btc private key",
+    r"[\s:]*([0-9a-zA-Z]{32})[\s]*": "btc address",
+    r"[\s:]*([59][0-9a-zA-Z]{50})[\s]*": "Base58 Wallet Import format"
+}
 
-pastbin_listing_url = "https://scrape.pastebin.com/api_scraping.php"
-pastbin_listing_params = {"limit":"100"}
-pastbin_scrape_url = "https://scrape.pastebin.com/api_scrape_item.php?i="
+pastebin_listing_url = "https://scrape.pastebin.com/api_scraping.php"
+pastebin_listing_params = {"limit":"100"}
+pastebin_scrape_url = "https://scrape.pastebin.com/api_scrape_item.php?i={}"
 
 
 def main():
@@ -28,13 +30,14 @@ def main():
         old_listing = current_listing
 
         for item in new_elements:
-            threading.Thread(target=parse_paste, args=(item,), daemon=True)
+            new_thread = threading.Thread(target=parse_paste, args=(item,), daemon=True)
+            new_thread.start()
 
         time.sleep(1) #sleep for a second no matter what. pastes come in slow most of the time
 
 def get_updates():
     ret = set()
-    listing = requests.get(url=pastbin_listing_url, params=pastbin_listing_params)
+    listing = requests.get(url=pastebin_listing_url, params=pastebin_listing_params)
     if listing.status_code == 200:
         pastes = json.loads(listing.text)
         for items in pastes:
@@ -43,9 +46,31 @@ def get_updates():
         raise ConnectionError("Status code: {}".format(listing.status_code))
     return ret
 
-def parse_paste():
-    pass
+def parse_paste(paste_id):
+    paste = get_paste(paste_id)
+    if not paste:
+        print("[-] Error: Unable to retrieve paste {}".format(paste_id))
+        return None #currently we just fail if we have a problem when trying to get a paste
+    results = search_paste(paste)
+    if results:
+        send_results(results)
 
+    return None
+
+def get_paste(paste_id):
+    try:
+        paste = requests.get(url=pastebin_scrape_url.format(paste_id))
+        if paste.status_code == 200:
+            return paste.text
+    except Exception as e:
+        print("Connection error: {}".format(e))
+    return None #we get here if any kind of error occurred
+
+def search_paste(paste):
+    return None
+
+def send_results(results):
+    pass
 
 if __name__ == "__main__":
     main()
