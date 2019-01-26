69ba47f3081d4111619c59e094590e6e812536b2
linted <linted@users.noreply.github.com>
2019-01-26 14:28:37 -0500

    working towards a good product

--- scrape.rb
+++ scrape.rb
@@ -1,6 +1,6 @@
 
 
-class listing
+class Listing
     require 'net/http'
     require 'json'
     require 'set'
@@ -19,9 +19,8 @@ class listing
         ret = nil
         begin
             response = HTTP.get_response(@url)
-            listing = JSON.parse(@response) if response.is_a? HTTPSuccess
             tags = Set.new
-            listing.each {|x| tags.merge x['key']} if listing.is_a? Array
+            JSON.parse(@response).each {|x| tags.merge x["key"]} if response.is_a? HTTPSuccess
             ret = tags - @listing
             @listing = tags
         rescue ParserError
@@ -34,7 +33,7 @@ class listing
     
 end
 
-class scraper
+class Scraper
     @@pastebin_scrape_url = URL("https://scrape.pastebin.com/api_scrape_item.php")
     @@pastebin_scrape_params = {i: @listing_id}
 
@@ -46,23 +45,48 @@ class scraper
 
     public
     def get_paste
-        response = 
-
-
+        HTTP.get_response(@url)
+    end
 end
 
-class send
+class Send
+    def initialize title message
+        @title = title
+        @message = message
+    end
+
     def send
         post_paste
     end
 end
 
-class email < send
-    def initialize
+class Email < send
+    require 'net/smtp'
+
+    def initialize title, message, server, src_email, dst_email, password
+        super title, message
+        @password = password
+        @server = server
+        @src_email = src_email
+        @dst_email = dst_email
+
+        @email = <<END_OF_MESSAGE
+FROM: #{@src_email} <#{@src_email}>
+TO: listeners <#{@dst_email}>
+SUBJECT: #{@title}
+DATE: #{TIME.now}
+
+#{@message}
+
+END_OF_MESSAGE
     end
 
     private 
     def post_paste
-
+        SMTP.enable_starttls.start(@server, 587) do |smtp|
+            smtp.send_message @email, @src_email, @dst_email
+        end
     end
 end
+
+def main
