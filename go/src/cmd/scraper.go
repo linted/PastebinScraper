0ca0bb1a5c68823e4ae1a2819f7ef43181099f6e
linted <linted@users.noreply.github.com>
2019-05-02 08:57:49 -0400

    started work on a filter function which will work as an intersection

--- go/src/cmd/scraper.go
+++ go/src/cmd/scraper.go
@@ -14,10 +14,26 @@ type paste struct {
 	contents []byte
  }
 
+type listing struct {
+	Scrape_url string `json:"scrape_url"`
+	Full_url string `json:"full_url"`
+	Date string `json:"date"`
+	Key string `json:"key"`
+	Size string `json:"size"`
+	Expire string `json:"expire"`
+	Title string `json:"title"`
+	Syntax string `json:"syntax"`
+	User string `json:"user"`
+}
+
+type listings []listing
+
 var scrapeAmount := 30
 var pastebinURL := "https://pastebin.com"
 var scrapePath := fmt.Sprintf("%s/api_scraping.php?limit=%d", pastebinURL, scrapeAmount) //We don't change the limit, so compile it once and be done
 var fetchPath, fetechPathError := url.Parse(fmt.Sprintf("%s/api_scrape_item.php")) //The query string on this one changes a lot so do it as needed
+
+func filter()
 
 func getPaste(pasteID string, queue chan paste) {
 	log.Printf("Fetching paste: %s", pasteID)
@@ -45,12 +61,19 @@ func getPaste(pasteID string, queue chan paste) {
 	return
 }
 
+func filterRecent() error {
+	
+}
+
+
 func scrape(pasteQueue chan paste, stop chan bool) {
 	if fetechPathError != nil {
 		log.Panicf("Oh no! your pastebin url is messed up. check it! %s", fetechPathError)
 	}
 	log.Print("Starting to scrape!\n")
 	
+	var recentPastes := make(map[string]listing)
+
 	foreverLoop: for {
 		select {
 		case <- stop:
@@ -60,7 +83,9 @@ func scrape(pasteQueue chan paste, stop chan bool) {
 			if err != nil {
 				log.Print("Error while scraping: %s", err)
 			}
-			
+
+
+
 		}
 	}	
 
