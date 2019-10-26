6f0bb5381a1095a1e1f46a09e0102c91f560bdbc
linted <linted@users.noreply.github.com>
2019-10-26 12:41:01 -0400

    started using the elastic client. Hopefully this does everything we need

--- go/build.sh
+++ go/build.sh
@@ -12,7 +12,7 @@ cd ..
 
 #start building go-yara
 export GOPATH=$(pwd):$GOPATH
-export export PKG_CONFIG_PATH=$(pwd)/libyara/libyara/:$(go env PKG_CONFIG_PATH)
+export PKG_CONFIG_PATH=$(pwd)/libyara/libyara/:$(go env PKG_CONFIG_PATH)
 export CGO_CFLAGS="-I$(pwd)/libyara/libyara/include/ $(go env CGO_CFLAGS)" 
 export CGO_LDFLAGS="-L$(pwd)/libyara/libyara/.libs -lm $(go env CGO_LDFLAGS) -lcrypto"
 go get -tags yara_static github.com/hillu/go-yara
@@ -21,6 +21,10 @@ go test github.com/hillu/go-yara
 if [ $? -ne 0 ]; then
     echo "__________________________BUILD ERROR__________________________"
     go env
+fi
+
+if [ ! -d $GOPATH/src/github.com/elastic/go-elasticsearch ]; then
+    git clone --branch master https://github.com/elastic/go-elasticsearch.git $GOPATH/src/github.com/elastic/go-elasticsearch
 fi
 
 go build -o scraper -race cmd/*.go
--- go/cmd/elastic.go
+++ go/cmd/elastic.go
@@ -7,16 +7,19 @@ import (
 	"io/ioutil"
 	"log"
 	"net/url"
+	"strings"
+
+	elasticsearch "github.com/elastic/go-elasticsearch"
 )
 
-var elasticURL elasticConfig
+var elasticURLs elasticConfig
 
 type elasticConfig struct {
-	endpointURL string
+	endpoints []string
 }
 
 func (s *elasticConfig) String() string {
-	return s.endpointURL
+	return s.endpoints
 }
 
 func (s *elasticConfig) Set(arg string) error {
@@ -24,16 +27,19 @@ func (s *elasticConfig) Set(arg string) error {
 		return errors.New("elastic config flag already set")
 	}
 
-	elasticURL, err := ioutil.ReadFile(arg)
+	configs, err := ioutil.ReadFile(arg)
 	if err != nil {
-		log.Panicf("failed while reading in elastic config: %s", err)
+		log.Panicf("failed while reading in elastic config file: %s", err)
 	}
 
-	tmpURL, err := url.Parse(string(elasticURL))
-	if err != nil {
-		return fmt.Errorf("failed while trying to parse elastic url from config file: %s", err)
+	urls := strings.Fields(configs)
+	for _, elasticURL := range urls {
+		tmpURL, err := url.Parse(string(elasticURL))
+		if err != nil {
+			return fmt.Errorf("failed while trying to parse elastic url from config file: %s", err)
+		}
+		s.endpoints = append(s.endpoints, tmpURL.String())
 	}
-	s.endpointURL = tmpURL.String()
 	return nil
 }
 
@@ -49,4 +55,10 @@ func validateElasticFlags() {
 
 func postToElastic(sendQueue chan pasteMatch) {
 	log.Println("Starting Elastic logging")
+
+	client, err := elasticsearch.NewClient()
+	if err != nil {
+		log.Fatalf("unable to create an elastic client:", err)
+	}
+	log.Printf("Configured urls = %q", elasticURLs.endpoints)
 }
